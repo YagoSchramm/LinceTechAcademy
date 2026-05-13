a747dc4028da61994b1bd9afa78616cc247d9308
YagoSchramm <yrtoiu1515@gmail.com>
2026-05-13 13:52:25 -0300

    ap2 coleções

---
+++ ap2_colecoes/ap2.dart
@@ -0,0 +1,14 @@
+import 'dart:math';
+
+void main(List<String> args) {
+  final random = Random();
+  List<int> listaOriginal = [];
+  for (var i = 0; i < 50; i++) {
+    listaOriginal.add(random.nextInt(100));
+  }
+  List<int> listaAtualizada = List.from(listaOriginal);
+
+  listaAtualizada.removeWhere((num) => num % 2 == 0);
+  print("lista original: $listaOriginal");
+  print("lista atualizada : $listaAtualizada");
+}
